91a7ae3991e68c6cacdbe87107575f9f3438c6f5
Gurii09 <Gurleen.kaur02@edu.sait.ca>
2022-09-29 17:19:59 -0600

    new

--- Assignment 1/src/sait/bms/application/Application.java
+++ Assignment 1/src/sait/bms/application/Application.java
@@ -1,48 +1,5 @@
 package sait.bms.application;
 
-import java.io.*;
-import java.util.*;
-//9791111299254
-
-import sait.bms.manager.Book;
-import sait.bms.problemdomain.ChildrensBook;
-
 public class Application {
-
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-
-		System.out.printf("Welcome to ABC Book Company: How May We Assist You?\r\n" +
-				"1	Checkout Book%n" +
-				"2	Find Books by Title\r\n" +
-				"3	Display Books by Type\r\n" +
-				"4	Produce Random Book List\r\n" +
-				"5	Save & Exit\r\n" +
-				"");
-
-		System.out.printf("Enter option: ");
-		Scanner sc = new Scanner(System.in);
-		int input1 = sc.nextInt();
-
-		if (input1 == 1) {
-			Book.CheckOut();
-		}
-
-		if (input1 == 2) {
-			Book.FindBook();
-		}
-
-		if (input1 == 3) {
-			Book.DisplayBook();
-		}
-
-		if (input1 == 4) {
-			Book.RandomList();
-		}
-
-		if (input1 == 5) {
-			Book.Exit();
-		}
-	}
 
 }
---
+++ Assignment 1/src/sait/bms/problemdomain/Childrens_books.java
@@ -0,0 +1,10 @@
+package sait.bms.problemdomain;
+
+public class Childrens_books {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
