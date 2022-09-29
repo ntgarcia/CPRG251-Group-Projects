10bd77983617f070564951c27e4b2a2056294f7f
Piriya-K <piriya619@gmail.com>
2022-09-29 14:50:06 -0600

    Application and Book class

---
+++ Assignment 1/src/Book.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.*;
+
+public class Book {
+	String ISBN;
+	String callNumber;
+	int available;
+	int total;
+	String title;
+	
+	//No-arg constructor
+	public Book() {
+	}
+	
+	public Book(String ISBN, String callNumber, int available, int total, String title) {
+		this.ISBN = ISBN;
+		this.callNumber = callNumber;
+		this.available = available;
+		this.total = total;
+		this.title = title;
+	}
+	
+
+
+
+	@Override
+	public String toString() {
+		return "Book [ISBN=" + ISBN + ", callNumber=" + callNumber + ", available=" + available + ", total=" + total
+				+ ", title=" + title + "]";
+	}
+	
+	
+}
