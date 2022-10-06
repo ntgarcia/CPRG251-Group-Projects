50d07933c1cd2e4ae07be915fca0f0e6e9a16f0e
Nathan Garcia <n8g423@gmail.com>
2022-10-06 15:01:32 -0600

    stuff

--- Assignment 2/src/sait/frms/application/AppDriver.java
+++ Assignment 2/src/sait/frms/application/AppDriver.java
@@ -1,6 +1,10 @@
 package sait.frms.application;
 
 import sait.frms.gui.MainWindow;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import sait.frms.gui.*;
 
 /**
@@ -11,9 +15,24 @@ public class AppDriver {
 
 	/**
 	 * Entry point to Java application.
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		try {
+			// Set System L&F
+			UIManager.setLookAndFeel(
+					UIManager.getSystemLookAndFeelClassName());
+
+		} catch (UnsupportedLookAndFeelException e) {
+			// handle exception
+		} catch (ClassNotFoundException e) {
+			// handle exception
+		} catch (InstantiationException e) {
+			// handle exception
+		} catch (IllegalAccessException e) {
+			// handle exception
+		}
 		MainWindow mainWindow = new MainWindow();
 		mainWindow.display();
 	}
--- Assignment 2/src/sait/frms/gui/FlightsTab.java
+++ Assignment 2/src/sait/frms/gui/FlightsTab.java
@@ -12,97 +12,111 @@ import sait.frms.problemdomain.Flight;
  * Holds the components for the flights tab.
  * 
  */
-public class FlightsTab extends TabBase 
-{
+public class FlightsTab extends TabBase {
 	/**
 	 * Instance of flight manager.
 	 */
 	private FlightManager flightManager;
-	
+
 	/**
 	 * Instance of reservation manager.
 	 */
 	private ReservationManager reservationManager;
-	
+
 	/**
 	 * List of flights.
 	 */
 	private JList<Flight> flightsList;
-	
+
 	private DefaultListModel<Flight> flightsModel;
-	
+
 	/**
 	 * Creates the components for flights tab.
 	 */
 	/**
 	 * Creates the components for flights tab.
 	 * 
-	 * @param flightManager Instance of FlightManager.
+	 * @param flightManager      Instance of FlightManager.
 	 * @param reservationManager Instance of ReservationManager
 	 */
 	public FlightsTab(FlightManager flightManager, ReservationManager reservationManager) {
 		this.flightManager = flightManager;
 		this.reservationManager = reservationManager;
-		
+
 		panel.setLayout(new BorderLayout());
-		
+
 		JPanel northPanel = createNorthPanel();
 		panel.add(northPanel, BorderLayout.NORTH);
-		
+
 		JPanel centerPanel = createCenterPanel();
 		panel.add(centerPanel, BorderLayout.CENTER);
 	}
-	
+
 	/**
 	 * Creates the north panel.
+	 * 
 	 * @return JPanel that goes in north.
 	 */
-	private JPanel createNorthPanel() 
-	{
+	private JPanel createNorthPanel() {
 		JPanel panel = new JPanel();
-		
+
 		JLabel title = new JLabel("Flights", SwingConstants.CENTER);
-		title.setFont(new Font("serif", Font.PLAIN, 29));
+		title.setFont(new Font("Verdana", Font.PLAIN, 29));
 		panel.add(title);
-		
+
 		return panel;
 	}
-	
+
 	/**
 	 * Creates the center panel.
+	 * 
 	 * @return JPanel that goes in center.
 	 */
-	private JPanel createCenterPanel() 
-	{
+	private JPanel createCenterPanel() {
 		JPanel panel = new JPanel();
-		
+
 		panel.setLayout(new BorderLayout());
-		
+
 		flightsModel = new DefaultListModel<>();
 		flightsList = new JList<>(flightsModel);
-		
+
 		// User can only select one item at a time.
 		flightsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		
+
 		// Wrap JList in JScrollPane so it is scrollable.
 		JScrollPane scrollPane = new JScrollPane(this.flightsList);
-		
+
 		flightsList.addListSelectionListener(new MyListSelectionListener());
-		
+
 		panel.add(scrollPane);
-		
+
 		return panel;
 	}
-	
-	private class MyListSelectionListener implements ListSelectionListener 
-	{
+
+	/**
+	 * Creates the right panel.
+	 * 
+	 * @return JPanel that goes to the right.
+	 */
+	private JPanel createEastPanel() {
+		JPanel panel = new JPanel();
+
+		panel.setLayout(new GridBagLayout());
+
+		JLabel title = new JLabel("Reserve", SwingConstants.CENTER);
+		title.setFont(new Font("Verdana", Font.PLAIN, 25));
+
+		return panel;
+	}
+
+	private class MyListSelectionListener implements ListSelectionListener {
 		/**
 		 * Called when user selects an item in the JList.
 		 */
 		@Override
 		public void valueChanged(ListSelectionEvent e) {
-			
+
 		}
-		
+
 	}
 }
