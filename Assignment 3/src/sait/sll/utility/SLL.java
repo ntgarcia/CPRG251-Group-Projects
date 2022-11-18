bb14591ac6cebcc07babd07b15230dd6bcf3745b
Nathan Garcia <n8g423@gmail.com>
2022-11-17 17:11:26 -0700

    implemented code shown on 11/07

--- Assignment 3/src/sait/sll/utility/SLL.java
+++ Assignment 3/src/sait/sll/utility/SLL.java
@@ -2,6 +2,10 @@ package sait.sll.utility;
 
 public class SLL implements LinkedListADT  {
 
+	private SLLNode head;
+	private SLLNode tail;
+	private int size;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -22,19 +26,49 @@ public class SLL implements LinkedListADT  {
 	@Override
 	public void append(Object data) {
 		// Append an item to the end of the linked list.
+		SLLNode newNode = new SLLNode(data);
+		if(head == null) {
+			tail = head = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		} size++;
 		
 	}
 
 	@Override
 	public void prepend(Object data) {
 		// Prepend an item to the beginning of the linked list.
-		
+		if(head == null) {
+			append(data);
+		} else {
+			SLLNode newNode = new SLLNode(data);
+			newNode.next = head;
+			head = newNode;
+			size++;
+		}
 	}
 
 	@Override
 	public void insert(Object data, int index) throws IndexOutOfBoundsException {
 		// Insert an item at a specific index in the linked list.
-		
+		if(index < 0 || index > size) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (index == 0) {
+			prepend(data);
+		} else if (index >= size) {
+			append(data);
+		} else {
+			SLLNode current = head;
+			for (int i = 1; i < index; i++) {
+				current = current.next;
+			}
+			SLLNode temp = current.next;
+			current.next = new SLLNode(data);
+			(current.next) .next = temp;
+			size++;
+		}
 	}
 
 	@Override
@@ -52,6 +86,9 @@ public class SLL implements LinkedListADT  {
 	@Override
 	public void delete(int index) throws IndexOutOfBoundsException {
 		// Remove an item at an index in the linked list.
+		if(index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
 		
 	}
 
@@ -71,6 +108,20 @@ public class SLL implements LinkedListADT  {
 	public boolean contains(Object data) {
 		// Check if linked list has an item.
 		return false;
+	}
+	
+	class SLLNode {
+		Object data;
+		SLLNode next;
+		
+		public SLLNode(Object data, SLLNode next) {
+			this.data = data;
+			this.next = next;
+		}
+		
+		public SLLNode(Object data) {
+			this.data = data;
+		}
 	}
 
 }
