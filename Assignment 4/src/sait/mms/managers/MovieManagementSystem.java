5969fbbad68d1626e6271f52202a50102f1fa34d
Nathan Garcia <n8g423@gmail.com>
2022-11-28 13:07:49 -0700

    method 3 and 4 start

--- Assignment 4/src/sait/mms/managers/MovieManagementSystem.java
+++ Assignment 4/src/sait/mms/managers/MovieManagementSystem.java
@@ -6,19 +6,17 @@ import java.util.*;
 import sait.mms.drivers.MariaDBDriver;
 
 public class MovieManagementSystem {
-	
+
 	public Scanner in;
 	private MariaDBDriver driver = new MariaDBDriver();
-	
-	
-	
+
 	public MovieManagementSystem() {
 		in = new Scanner(System.in);
 		displayMenu();
 	}
-	
+
 	public void displayMenu() {
-		
+
 		System.out.println("Jim's Movie Manager");
 		System.out.println("1     Add New Movie");
 		System.out.println("2     Print movies released in year");
@@ -41,20 +39,21 @@ public class MovieManagementSystem {
 				System.exit(0);
 		}
 	}
-	
+
 	private void addMovie() {
 		System.out.print("Enter movie title: ");
 		String title = in.next();
 		System.out.print("Enter duration: ");
 		int duration = in.nextInt();
+		in.next();
 		System.out.print("Enter year: ");
 		String year = in.next();
-		
-		String query = "INSERT INTO movies (title, duration, year) VALUES ('" + 
-		title + "', " +
-		duration + ", '" +
-		year + "')";
-		
+
+		String query = "INSERT INTO movies (title, duration, year) VALUES ('" +
+				title + "', " +
+				duration + ", '" +
+				year + "')";
+
 		try {
 			driver.update(query);
 			System.out.println("Added movie to database\r\n");
@@ -66,16 +65,43 @@ public class MovieManagementSystem {
 			displayMenu();
 		}
 	}
-	
+
 	private void printMoviesInYear() {
-		
+
 	}
-	
+
 	private void printRandomMovies() {
-		
+		System.out.print("Enter number of movies: ");
+		int numMovies = in.nextInt();
+
+		String query = "SELECT * FROM movies ORDER BY RAND() LIMIT " + numMovies;
+
+		try {
+			driver.get(query);
+			driver.disconnect();
+			displayMenu();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			displayMenu();
+		}
 	}
-	
+
 	private void deleteMovie() {
-		
+		System.out.print("Enter the movie ID that you want to delete: ");
+		int id = in.nextInt();
+
+		String query = "DELETE FROM movies WHERE id = '" + id + "'";
+
+		try {
+			driver.update(query);
+			System.out.println("Movie " + id + " is deleted.\r\n");
+			driver.disconnect();
+			displayMenu();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			displayMenu();
+		}
 	}
 }
